459ddf746e3246e85e0bfef8d315f22bda5ea56b
Alex T <trinh@bbe.io>
2019-01-05 13:17:50 +1100

    no message

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import './Person.scss';
+import styles from './Person.scss';
+import './Demo.scss';
 
 const person = (props) => {
 
     return (
-        <div className="Person">
+        <div className={styles.Person}>
             <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old! </p>
             {props.children}
             <input type="text" onChange={props.changed} value={props.name} />
